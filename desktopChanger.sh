a8851588623107303ab01c54284d190e6b04482a
Lucas Milotich <lucasmilotich@imac.cpe.telecentro.net.ar>
2018-03-05 20:11:33 -0300

    remove old cron

--- desktopChanger.sh
+++ desktopChanger.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 curl -o /tmp/image.jpg -k  https://raw.githubusercontent.com/LucasMilotich/bashJokes/master/funnyPNGs/SpySquad_616x385_LAM_tcm893-118336.jpg 
 
 echo "0/10 * * * * osascript -e 'tell application \"Finder\" to set desktop picture to POSIX file \"/tmp/image.jpg\"" >> funnyCron
